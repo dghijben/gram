3bd45f918dd61454c65f5b84527386e137b93e80
Dave Ghijben <dghijben@gmail.com>
2013-10-26 11:39:13 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tokyogram::Application.config.secret_key_base = 'f72c11444ae2814e2c03694304c0131765221f2e9b3a3797a8c1216aba87e9cd9cac6d3df731c53fb3d50afdb8b29c4e6545211af443a8712c931a38efdc89b0'
